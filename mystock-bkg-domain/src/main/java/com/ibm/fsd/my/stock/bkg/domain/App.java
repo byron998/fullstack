93d0869f328a7c0bd92ba99e3d2bbdb745cff488
chenxiaobin <chenxiaobin@LAPTOP-MQQAG9SJ>
2020-04-25 21:34:39 +0800

    Commit for mybatis generator.

---
+++ mystock-bkg-domain/src/main/java/com/ibm/fsd/my/stock/bkg/domain/App.java
@@ -0,0 +1,13 @@
+package com.ibm.fsd.my.stock.bkg.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Run mvn build mybatis-generator:generate" );
+    }
+}
