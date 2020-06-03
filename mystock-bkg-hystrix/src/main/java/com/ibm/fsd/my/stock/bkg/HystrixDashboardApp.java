bafe316e297f55d41f6ee49cc07ca3770388032c
chenxiaobin <chenxiaobin@LAPTOP-MQQAG9SJ>
2020-06-03 09:38:16 +0800

    Commit for hystrix ready.

---
+++ mystock-bkg-hystrix/src/main/java/com/ibm/fsd/my/stock/bkg/HystrixDashboardApp.java
@@ -0,0 +1,13 @@
+package com.ibm.fsd.my.stock.bkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@SpringCloudApplication
+public class HystrixDashboardApp {
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApp.class, args);
+	}
+}
